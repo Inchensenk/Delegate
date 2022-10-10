68c97f3e54aceb82c4d13b482637ecfa6989c3b0
Inchensenk <aab8117@gmail.com>
2022-10-10 16:25:26 +0300

    Лямбда-оператор - это многооператорное выражение-лямбда, а выражение-лямбда - это однооператорный лямбда-оператор.

---
+++ LambdaOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LambdaOperators
+{
+    public delegate void HelloDelegate(string s);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloDelegate helloDelegate = (x) => { Console.WriteLine("Привет, " + x); };
+            helloDelegate("Андрей");
+        }
+    }
+}
---
+++ LambdaOperators1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LambdaOperators1
+{
+    public delegate int SubDelegate(int a, int b);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             //Делегат объявлен по сильной ссылке
+            SubDelegate subDelegate = (x, y)=> 
+            { 
+                int result = x-y;
+
+                return result; 
+            };
+            */
+
+            //делегат объявлен по слабой ссылке
+            int c = new SubDelegate((x, y) =>
+            {
+                int result = x - y;
+
+                return result;
+            }).Invoke(10,8);
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+ Лямбда-оператор - это многооператорное выражение-лямбда, а выражение-лямбда - это однооператорный лямбда-оператор. 
+ */
