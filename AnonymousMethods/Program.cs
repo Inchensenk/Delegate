ea6a35e6644d07b96fb39938304e3ff6fc131cbd
Inchensenk <aab8117@gmail.com>
2022-10-10 15:05:01 +0300

    Анонимные методы
    https://www.youtube.com/watch?v=3RSpVX3vEMQ

---
+++ AnonymousMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+    public class Program
+    {
+        public delegate void MyDelegate();
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = new MyDelegate(delegate () { Console.WriteLine("Hello World!"); });
+
+            myDelegate();// или myDelegate.Invoke()
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AnonymousMethods1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AnonymousMethods1
+{
+    public class Program
+    {
+        public delegate int AddDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            AddDelegate addDelegate = new AddDelegate(delegate (int number1, int number2) 
+            { 
+                int result = number1 + number2;
+                return result;
+            });
+
+            int summ = addDelegate(10,26);
+            Console.WriteLine(summ);
+            Console.ReadKey();
+        }
+    }
+}
