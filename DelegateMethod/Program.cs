d4e6868747ad202c214351697df11f4ee382bdd5
Inchensenk <aab8117@gmail.com>
2022-10-10 14:02:57 +0300

    Сообщение метода с делегатом
    https://www.youtube.com/watch?v=JHvrTgjMgxo&list=PLH3y3SWteZd2kB92hHtqMnLNZrKQVo9Up&index=58

---
+++ DelegateMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace DelegateMethod
+{
+    public delegate string HelloDelegate(string name);
+
+    class SomeClass
+    {
+        public string SayHello(string s)
+        {
+            return $"Привет, {s}";
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+
+            //Сообщаем делегат helloDelegate с методом SayHello
+            HelloDelegate helloDelegate = new HelloDelegate(someClass.SayHello);
+            
+            //Вывод на консоль результат вызова делегата//
+
+            //короткая запись
+            Console.WriteLine(helloDelegate("ПЕТР"));
+
+            //длинная запись
+            Console.WriteLine(helloDelegate.Invoke("ПЕТР"));
+
+            Console.ReadKey();
+        }
+    }
+}
