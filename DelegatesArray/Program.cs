72635a6707b59af2532e1ab6d6750ed795768352
Inchensenk <aab8117@gmail.com>
2022-10-11 15:40:53 +0300

    C#. Массив методов (массив делегатов). Урок 57
    https://www.youtube.com/watch?v=wb_Gsll4R6U&list=PLH3y3SWteZd2kB92hHtqMnLNZrKQVo9Up&index=62

---
+++ DelegatesArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DelegatesArray
+{
+
+    public delegate void MyDelegate(string s);
+    internal class Program
+    {
+        private static void SayHello(string str)
+        {
+            Console.WriteLine("Hello, " + str);
+        }
+        static void Main(string[] args)
+        {
+            //массив делегатов
+            MyDelegate[] delsArray = new MyDelegate[10];
+
+            /*
+            for (int i = 0; i < delsArray.Length; i++)
+            {
+                delsArray[i] = new MyDelegate(SayHello);
+            }
+            */
+
+            for (int i = 0; i < delsArray.Length; i++)
+            {
+                delsArray[i] = SayHello;
+            }
+
+            for (int i = 0; i < delsArray.Length; i++)
+            {
+                delsArray[i](Convert.ToString(i));// или delsArray[i].Invoke(Convert.ToString(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DelegatesArray1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DelegatesArray1
+{
+    public delegate void MyDelegate(string s);
+    internal class Program
+    {
+        private static void SayHello(string str)
+        {
+            Console.WriteLine("Hello, "+str);
+        }
+
+        private static void SayGoodbye(string str)
+        {
+            Console.WriteLine("Goodbye, " + str);
+        }
+
+        private static void AskHowAreYou(string str)
+        {
+            Console.WriteLine($"How Are You, {str}?");
+        }
+
+        static void Main(string[] args)
+        {
+            //инициализация с помощью ссылок на методы
+            /*MyDelegate[] methodsArray = new MyDelegate[]
+            {
+                SayHello, SayGoodbye, AskHowAreYou
+            };*/
+
+            //инициализация с помощью анонимных методов
+            MyDelegate[] methodsArray = new MyDelegate[]
+            {
+                //анонимный метод
+                delegate (string s)
+                {
+                    Console.WriteLine($"Hello, {s}");
+                },
+
+                //лямбда оператор
+                (s) =>
+                {
+                    Console.WriteLine($"Goodbye, {s}");
+                },
+
+                //выражение лямбда
+                (s)=>Console.WriteLine($"How Are You, {s}?")
+            };
+         
+
+            for (int i = 0; i < methodsArray.Length; i++)
+            {
+                methodsArray[i](Convert.ToString(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
