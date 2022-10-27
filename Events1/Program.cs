a7fb3abff765ac75efa246f596d84e5a75054efa
Inchensenk <aab8117@gmail.com>
2022-10-27 17:01:50 +0300

    C#. События / Обработка и создание / event. Урок 66
    https://www.youtube.com/watch?v=6vP9FsWJ2IM&list=PLH3y3SWteZd2kB92hHtqMnLNZrKQVo9Up&index=72

---
+++ Events1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Events1
+{
+    public delegate void EventDelegate();
+    public class MyClass
+    {
+        public event EventDelegate myEvent = null;
+
+        //add и remove это ключевые слова доступа для событий они делают работу с событиями более безопаснее
+        public event EventDelegate MyEvent
+        {
+            //подписка
+            add { myEvent += value; }
+            //отписка
+            remove { myEvent -= value; }
+        }
+        public void InvokeEvent()
+        {
+            myEvent.Invoke();
+        }
+    }
+    public class Program
+    {
+        //Методы обработки события
+
+        static private void Handler1()
+        {
+            Console.WriteLine("Обработчик события 1");
+        }
+
+        static private void Handler2()
+        {
+            Console.WriteLine("Обработчик события 2");
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            MyClass instance = new MyClass();
+
+            instance.myEvent += new EventDelegate(Handler1);
+            instance.myEvent += Handler2;
+
+            instance.InvokeEvent();
+
+            Console.WriteLine("---");
+            instance.myEvent -= new EventDelegate(Handler2);
+            instance.InvokeEvent();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Events2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Events2
+{
+    
+    public delegate void KeyPressEventDelegate();
+
+    /// <summary>
+    /// Класс издатель
+    /// </summary>
+    public class KeyboardMaster
+    {
+        public event KeyPressEventDelegate wKeyPressedEvent = null!;
+        public event KeyPressEventDelegate sKeyPressedEvent = null!;
+
+        public void WKeyPressedEvent()
+        {
+            //если на событие что то подписанно то мы его вызываем
+            if(wKeyPressedEvent != null)
+                wKeyPressedEvent.Invoke();
+        }
+
+        public void SKeyPressedEvent()
+        {
+            if (sKeyPressedEvent != null)
+                sKeyPressedEvent.Invoke();
+        }
+    }
+
+    /// <summary>
+    /// Класс обработчик
+    /// </summary>
+    public class Program
+    {
+        
+        //обработчики нажатия на кнопки
+        static private void buttonW_Click()
+        {
+            Console.Clear();
+            Console.WriteLine("\nВперед");
+        }
+
+        static private void buttonS_Click()
+        {
+            Console.Clear();
+            Console.WriteLine("\nНазад");
+        }
+
+
+        static void Main(string[] args)
+        {
+            KeyboardMaster master = new KeyboardMaster();
+            master.wKeyPressedEvent += buttonW_Click;
+            master.sKeyPressedEvent += buttonS_Click;
+
+            //enum который содержит в себе все кнопки клавиатуры
+            ConsoleKey pressedKey;
+
+            //Имитация открытого окна интерфейсного приложения
+            while (true)
+            {
+                //получаем нажатую кнопку, если она была нажата
+                pressedKey = Console.ReadKey().Key;
+
+                switch (pressedKey)
+                {
+                    //если нажата клавиша W, то срабатывают все методы подписанные на событие wKeyPressedEvent
+                    case ConsoleKey.W:
+                        master.WKeyPressedEvent();
+                        break;
+
+                    //если нажата клавиша S, то срабатывают все методы подписанные на событие sKeyPressedEvent
+                    case ConsoleKey.S:
+                        master.SKeyPressedEvent();
+                        break;
+                }
+            }
+        }
+    }
+}
+
+/*
+ По сути интерфейсные приложения это в бесконечном цикле работающее окно, 
+оно непрерывно ждет каких-то действий со стороны пользователя, 
+непрерывно слушает действия пользователя чтобы их как-то обработать
+
+ */
